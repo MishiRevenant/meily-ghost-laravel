fb4ac192f7cfa736606371a788cd4a16d0367ce3
MishiRevenant <jm5372533@gmail.com>
2025-11-03 16:04:47 -0500

    en la ruta del pastel

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TiendaController; // Importamos nuestro controlador
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect; // 
 
 /*
 |--------------------------------------------------------------------------
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',     // Archivo de Breeze
+                'resources/css/style.css',   // ¡Tu archivo!
+                'resources/js/app.js',       // Archivo de Breeze
+                'resources/js/sidebar.js', // ¡Tu archivo!
+                'resources/js/tienda.js',    // ¡Tu archivo!
+            ],
             refresh: true,
         }),
     ],
