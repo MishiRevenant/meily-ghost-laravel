b09a32da07bfa11af231f9f0edda5153922a02e6
MishiRevenant <jm5372533@gmail.com>
2025-11-02 21:03:48 -0500

    eso

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,55 +6,26 @@
 @section('header-subtitle', 'Donde lo oscuro se encuentra con lo adorable.')
 
 @section('content')
-<section class="main-section">
-    <h2>Novedades</h2>
-    <p>Aquí puedes poner una galería o los últimos productos añadidos.</p>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meily Ghost | Pulseras Únicas</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" href="https://scontent.fcuz2-1.fna.fbcdn.net/v/t39.30808-6/476437022_122116658288648912_4755061113525152841_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHOVONoddacloSa-9euEaNd6M0UMd6JblLozRQx3oluUgIBmybeAJPz3kU1-LY_gvbdVqGO-wNWDqIssLlwIIzX&_nc_ohc=T514HRelql0Q7kNvwGvM2FU&_nc_oc=AdlT8wRwiJuMjNNW807PuWouP_3WZrI3LSpRs_rNzWVP0w8_GI9CWZN4B_C2fWgbqZo&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=aZ2cD_78IJEfvvTr3YeoEQ&oh=00_AfYZFoPprW7hMg5uHqnwnKPYWVIMiGHmWpHgrX4jIPRgaw&oe=68DECCF3" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-</head>
-<body>
-<nav id="sidebar" class="sidebar">
-    <a href="index.html" class="active"><i class="fas fa-home"></i><span>Inicio</span></a>
-    <a href="tienda.php"><i class="fas fa-store"></i><span>Tienda</span></a>
-    <a href="inspiracion.html"><i class="fas fa-lightbulb"></i><span>Inspiración</span></a>
-    <a href="acerca.html"><i class="fas fa-info-circle"></i><span>Acerca de</span></a>
-    <a href="contacto.php"><i class="fas fa-envelope"></i><span>Contacto</span></a>
-    <a href="admin.php"><i class="fas fa-user-shield"></i><span>Admin</span></a>
-</nav>
-<header>
-    <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t39.30808-6/468282342_586989107106293_9188685479668068517_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGqaBqQuSIFdrSGvSeCK_SngyhqWNOuwTqDKGpY067BOvLpiHjJtqPjYQUTvRZUJhLMmiFs40xKKr6rZNwmCsUw&_nc_ohc=3bEk8xeZqmYQ7kNvwGefWmA&_nc_oc=AdmmvKQnCo2mKYnal3r9CuTk6eUCw43GxIoC50uF6AZydtUGiJxEAeZAhZbh35Oj1dQ&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=2d1C21sehre24ha-Q3TTLw&oh=00_AfYTjjRgGKhTZebiABbwRUNgh62Jq5s0xd4jeQ6X4XglZA&oe=68DE986D">
-    <h1>Meily Ghost</h1>
-    <p class="intro-coraline">Creaciones alternativas y mágicas hechas a mano en Cusco.</p>
-</header>
 
+{{-- Slider de imágenes --}}
 <section id="slider" class="slider-coraline">
     <div class="slide active">
         <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t51.82787-15/515013669_17879251866349154_4108438826815419407_n.webp?stp=dst-jpg_tt6&_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF3RHaemwAfGEGjT-8HgBefRnBDoHnVmN1GcEOgedWY3ZLRhObtSqOCTcTSlEz3sW81_O1QYtXxwUHLhWuvllWL&_nc_ohc=aPDqsbwJsk4Q7kNvwG2vwuJ&_nc_oc=AdlvvBUZACpVj7rtiaz06DthPd8fYyKeEcROhsTKIDCKPlyWdA6RSB_DKizt4lVzUlo&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=Z91KKIfNrE2kssSA5VEkoA&oh=00_AfaaW7eTkuFLpq59zjrPjYqvjsSJjEv_rFtK8Xdx2LjaKA&oe=68DFACDB" alt="Pulsera 1">
         <div class="caption">✨ Pulseras con alma propia ✨</div>
     </div>
-    <div class="slide active">
-        <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t51.75761-15/503282498_17876157765349154_4596086329428771386_n.webp?stp=dst-jpg_tt6&_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEd0llHIafq_fh1k1TRjGhPJsTtZ3PnlQwmxO1nc-eVDAwTXszapZPAA6aP710uqgccDA8PYAaogskDOe25dLeX&_nc_ohc=49D1aHjMr0IQ7kNvwEX_iQc&_nc_oc=AdklCvV7RCZ0Ya2OZ9ya7CDhN9JVIXvXZ5gp8bCVXdt6gdLEeM-roIc6TzuSgRVcDQE&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=qslmdGVopDASgsyfCiOu7A&oh=00_AfYCBf21UHIXjsCRFTJDuU7jk5hJbZ9O7BNesyOVfcfHNA&oe=68DFBA9F" alt="Pulsera 1">
-        <div class="caption">✨ Pulseras con alma propia ✨</div>
-    </div>
     <div class="slide">
-        <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t51.75761-15/504337733_17876018121349154_6884709821534823017_n.webp?stp=dst-jpg_tt6&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF1qELMvXg0oZTzD340NWKyDR-cjwgxWkANH5yPCDFaQI2qiLKiu-5SUuPJf6NWOXdn1l7QYveazRdttEvGapw6&_nc_ohc=VI4LJ-7Nvr0Q7kNvwELIwMg&_nc_oc=Adl8MGgdE31b_s2W7Xtt7lhVrtC9a8jiZgNQNrn4FOUCxTCGpUYyVtFT91qSSoFy_jo&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=IctltY1HcJi7Wag4O2lqMQ&oh=00_Afb3corY4GLXmyZRB-sCEiz_Zt2NavefjhbJe_6u3JIJuQ&oe=68DFC032" alt="Pulsera 1">
+        <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t51.75761-15/504337733_17876018121349154_6884709821534823017_n.webp?stp=dst-jpg_tt6&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF1qELMvXg0oZTzD340NWKyDR-cjwgxWkANH5yPCDFaQI2qiLKiu-5SUuPJf6NWOXdn1l7QYveazRdttEvGapw6&_nc_ohc=VI4LJ-7Nvr0Q7kNvwELIwMg&_nc_oc=Adl8MGgdE31b_s2W7Xtt7lhVrtC9a8jiZgNQNrn4FOUCxTCGpUYyVtFT91qSSoFy_jo&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=IctltY1HcJi7Wag4O2lqMQ&oh=00_Afb3corY4GLXmyZRB-sCEiz_Zt2NavefjhbJe_6u3JIJuQ&oe=68DFC032" alt="Pulsera 2">
         <div class="caption">Hechas a mano, inspiradas en lo místico</div>
     </div>
     <div class="slide">
-        <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t51.75761-15/503526400_17875208925349154_3739101742277593119_n.webp?stp=dst-jpg_tt6&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFogur6eDHxUIBtlEwML1g_mWr_cTwXO0iZav9xPBc7SFP2fdhAwaKIy8cy5pcG7aa1Wwqr_MDZ2jUPH_DuVLO5&_nc_ohc=LjM8p-qXLKkQ7kNvwEk_XSS&_nc_oc=AdkDD1t-JqoYVMJHAGgrcb4kaBE7QUViMjSfHt3sK8fdpTLIQP7mve9iXkIEc5cvvR4&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=S2ZHw38URg7QAjujg-ej8g&oh=00_AfZn7Do7boyuIFHA92tuPAw7BDDFTcXHegpNLvF4bPh1FQ&oe=68DFB646" alt="Pulsera 1">
+        <img src="https://scontent.fcuz2-1.fna.fbcdn.net/v/t51.75761-15/503526400_17875208925349154_3739101742277593119_n.webp?stp=dst-jpg_tt6&_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFogur6eDHxUIBtlEwML1g_mWr_cTwXO0iZav9xPBc7SFP2fdhAwaKIy8cy5pcG7aa1Wwqr_MDZ2jUPH_DuVLO5&_nc_ohc=LjM8p-qXLKkQ7kNvwEk_XSS&_nc_oc=AdkDD1t-JqoYVMJHAGgrcb4kaBE7QUViMjSfHt3sK8fdpTLIQP7mve9iXkIEc5cvvR4&_nc_zt=23&_nc_ht=scontent.fcuz2-1.fna&_nc_gid=S2ZHw38URg7QAjujg-ej8g&oh=00_AfZn7Do7boyuIFHA92tuPAw7BDDFTcXHegpNLvF4bPh1FQ&oe=68DFB646" alt="Pulsera 3">
         <div class="caption">¡Personaliza tu propia pulsera!</div>
     </div>
     <button id="prev">&#10094;</button>
     <button id="next">&#10095;</button>
 </section>
 
+{{-- Sección de productos destacados --}}
 <section class="main-section">
     <h2>Productos Destacados</h2>
     <div class="products-grid">
@@ -76,12 +47,8 @@
     </div>
 </section>
 
-<footer class="footer">
-    &copy; 2025 Meily Ghost - Creaciones únicas hechas a mano
-</footer>
-
-<script src="sidebar.js"></script>
-<script src="slider.js"></script>
-</body>
-</html></section>
 @endsection
+
+@push('scripts')
+<script src="{{ asset('slider.js') }}"></script>
+@endpush
